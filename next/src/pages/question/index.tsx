e15c47861b9e675a60d1e70feeb0768f4dafd276
SuzakiKaito <114970408+SuzakiKaito@users.noreply.github.com>
2024-06-27 11:22:03 +0900

    問題解説画面　フロント

--- next/src/pages/question/index.tsx
+++ next/src/pages/question/index.tsx
@@ -1,7 +1,33 @@
+import React from 'react'
 import style from '@/styles/question/index.module.scss'
+import { useCallback,useEffect } from 'react'
 
 
-export default function home(){
+
+
+export default function question(){
+
+
+
+  // const handleClick = useCallback((e) =>{
+  //   console.log(e.target);
+  //   // style.changeColor.background = "#6ACFCF"
+  //   // e.preventDefault();
+  //   // alert(foo);
+  // },[])
+
+
+
+  // useEffect(() =>{
+  //   console.log("マウント時");
+  //   document.body.style.background = "tomato"
+    
+  //   return() =>{
+  //     console.log("アンマウント時");
+  //   document.body.style.background = ""
+  //   }
+  // },[])
+
   return(
     <>
       <div>
@@ -21,22 +47,29 @@ export default function home(){
                 <h4>レベル1</h4>
                 <h4>朝</h4>
               </div>
-              <nav className={style.gageContent}>
-                <ul>
-                  
+              {/* <nav>
+                <ul className={style.gageContent}>
+                  <li className={style.gage}></li>
+                  <li className={style.gage}></li>
+                  <li className={style.gage}></li>
+                  <li className={style.gage}></li>
                 </ul>
-              </nav>
+              </nav> */}
               <div className={style.question}>
                 <h3>お題お題お題お題でしょう？</h3>
                 <p>問題問題問題問題問題問題問題問題問題問題問題問題問題問題。</p>
               </div>
             </div>
             <div className={style.answerContent}>
-              <div className={style.answer}>
+              <div className={`${style.answer} ${style.circle}`}>
                 <a href="#">1.<span>休み時間</span></a>
               </div>
-              <div className={style.answer}>
-                <a href="#">1.<span>休み時間</span></a>
+              <div className={`${style.answer} ${style.changeColor}`}>
+                <a href="/explanation" 
+                // onClick={handleClick}
+                >
+                1.<span>休み時間</span>
+                </a>
               </div>
               <div className={style.answer}>
                 <a href="#">1.<span>休み時間</span></a>
